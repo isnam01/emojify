1c1124d82197ff789389fb69fe71f893a22b4410
isnam01 <guptamansi199901@gmail.com>
2020-12-09 00:56:40 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import React,{useState} from "react";
 import './App.css';
+import image from './images.png' ;
 
-function App() {
+const emojidirectory={
+  "😄":"happy",
+  "😂":"laughing",
+  "😡":"angry",
+  "😭":"crying",
+  "😔":"sad",
+  "😀":"grinning",
+  "🤣":"rolling laugh",
+  "😍":"heart eyes",
+  "😊":"blushing face",
+  "😉":"wynking face",
+  "🤪":"zany face",
+  "🤑":"money moth face",
+  "😑":"expressionless face",
+  "😶":"face without mouth",
+  "🤒":"face with thermometer",
+  
+}
+
+var emoji=Object.keys(emojidirectory);
+var values=Object.values(emojidirectory);
+
+export default function App() {
+
+  const [meaning,setmeaning]=useState("")
+
+  function emojihandler(event){
+    
+    var input=event.target.value;
+
+    console.log(typeof(input))
+    if(emoji.includes(input.toLowerCase()))
+    {
+      setmeaning(emojidirectory[input]);
+    }
+    else if(values.includes(input.toLowerCase()))
+    {
+      var key=Object.keys(emojidirectory).find(key => emojidirectory[key] === input)
+      setmeaning(key);
+    }
+  }
+
+  function emojiclickhandler(emo) {
+    setmeaning(emojidirectory[emo]);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{marginBottom:0}}>Emoji Finder</h1>
+      <h1>🔍😎</h1>
+      <h3>Search through word or emoji!</h3>
+      <input onChange={emojihandler}/>
+      <h2>{meaning}</h2>
+      <h3>
+        {
+          emoji.map((emo)=>{
+            return (
+              <span 
+              key={emo}
+                onClick={()=>emojiclickhandler(emo)}>
+                  {emo}
+              </span>
+            )
+          })
+        }
+      </h3>
     </div>
   );
 }
-
-export default App;
